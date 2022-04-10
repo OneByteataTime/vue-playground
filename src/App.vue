a9c1c5c0ceef1409c9bf585acbba5c915c075cf0
Steve <sabbajax@gmail.com>
2022-04-09 21:03:38 -0400

    Project creation

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Hello Vue App</h1>
+  <Registration />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Registration from "./components/Registration.vue"
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Registration
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Registration.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>Registration Form Example</h2>    
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        console.log("setup")    
+    },
+})
+</script>
