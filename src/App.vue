b82dac26ee75a5545bf6d18cd27a81a1abf0157f
Steve <sabbajax@gmail.com>
2022-04-16 15:43:16 -0400

    App that starts

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
 <template>
-  <h1>Hello Vue App</h1>
-  <Registration />
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import Registration from "./components/Registration.vue"
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: "App",
-  components: {
-    Registration
-  },
-});
-</script>
+  name: 'App',
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+
+  data () {
+    return {
+      //
+    }
+  },
+})
+</script>
